5a2bc06257be0dd763a0ccfe70bb3cbde532eb11
Marcos Mancia <marcosmancia77@gmail.com>
2026-07-01 16:13:47 -0700

    Implemented EXTI Driver

---
+++ Drivers/Inc/exti.h
@@ -0,0 +1,16 @@
+/**
+ * @file 		exti.h
+ * @brief 		Contains function prototypes EXTI driver
+ * @author 		Marcos E. Mancia Jr.
+ * @date 		2026-06-30
+ * @version 	1.0
+ */
+#ifndef EXTI_H_
+#define EXTI_H_
+
+/***** FUNCTION PROTOTYPES *****/
+void exti_init(void);
+
+void exti_test(void);
+
+#endif /* EXTI_H_ */
