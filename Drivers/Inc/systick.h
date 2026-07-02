c650658e6697e465b2ee6dbeb442b8c621eefde9
Marcos Mancia <marcosmancia77@gmail.com>
2026-07-01 19:51:19 -0700

    Added Systick Interrupt functionality

--- Drivers/Inc/systick.h
+++ Drivers/Inc/systick.h
@@ -2,13 +2,17 @@
  * @file 		systick.h
  * @brief 		Contains function prototypes for SysTick driver
  * @author 		Marcos E. Mancia Jr.
- * @date 		2026-06-30
- * @version 	1.2
+ * @date 		2026-07-01
+ * @version 	1.3
  */
 #ifndef SYSTICK_H_
 #define SYSTICK_H_
 
 /***** FUNCTION PROTOTYPES *****/
+void systickDelayMs(int delay);
+void systick_interrupt_init(void);
+
 void systick_test(void);
+void systick_interrupt_test(void);
 
 #endif /* SYSTICK_H_ */
--- Drivers/Src/systick.c
+++ Drivers/Src/systick.c
@@ -2,8 +2,8 @@
  * @file        systick.c
  * @brief       Tests SysTick-based blocking delay for STM32F411xe
  * @author      Marcos E. Mancia Jr.
- * @date        2026-06-30
- * @version     1.1
+ * @date        2026-07-01
+ * @version     1.2
  */
 #include "stm32f411xe.h"
 #include "systick.h"
@@ -15,13 +15,16 @@
 
 #define SYSTICK_LOAD_VAL		16000
 #define CTRL_ENABLE				(1U<<0)
+#define CTRL_TICKINT			(1U<<1)
 #define CTRL_CLKSRC				(1U<<2)
 #define CTRL_COUNTFLAG			(1U<<16)
+
+#define ONE_SEC_LOAD			16000000
 
 /***** TEST FUNCTIONS *****/
 
 /**
- * @brief  		Tests the SysTick timer by toggling an LED and
+ * @brief		Tests the SysTick timer by toggling an LED and
  * 		   		printing a message every second
  */
 void systick_test(void) {
@@ -39,6 +42,25 @@ void systick_test(void) {
 		/*Use SysTick peripheral to wait a second*/
 		systickDelayMs(1000);
 	}
+}
+
+/**
+ * @brief		Tests SysTick interrupt functionality
+ */
+void systick_interrupt_test(void) {
+	/*Enable CLK access to GPIOA*/
+	RCC->AHB1ENR |= GPIOAEN;
+
+	/* Enable General Purpose Output Mode for PA5*/
+	GPIOA->MODER |= (1U<<10);
+	GPIOA->MODER &= ~(1U<<11);
+
+	/*Initialize necessary peripherals for test*/
+	uart2_tx_init();
+	systick_interrupt_init();
+
+	/*Interrupt-based programming removes additional code from loop*/
+	while(1) {}
 }
 
 /***** HELPER FUNCTIONS *****/
@@ -64,4 +86,36 @@ void systickDelayMs(int delay) {
 
     /*Disable SysTick peripheral after delay*/
 	SysTick->CTRL = 0;
+}
+
+/**
+ * @brief		Generates a 1 Hz interrupt signal
+ */
+void systick_interrupt_init(void) {
+	/*Reload with number of clocks per millisecond*/
+	SysTick->LOAD = ONE_SEC_LOAD - 1;
+
+	/*Clear systick current value register*/
+	SysTick->VAL = 0;
+
+	/*Enable systick and select internal CLK src*/
+	SysTick->CTRL = CTRL_ENABLE | CTRL_CLKSRC;
+
+	/*Enable SysTick interrupt*/
+	SysTick->CTRL |= CTRL_TICKINT;
+}
+
+/**
+ * @brief		Prints message to terminal via UART and toggles LED
+ */
+void systick_callback(void) {
+	my_put("A second has passed...\n\r");
+	GPIOA->ODR ^= LED_PIN;
+}
+
+/**
+ * @brief       ISR for SysTick global interrupts
+ */
+void SysTick_Handler(void) {
+	systick_callback();
 }
